a415f015066fdeaa9cd53a78858344e22b2b0a08
Faith Odiwuor <odiwuorfaith024@gmail.com>
2026-08-20 13:08:19 -0700

    Faith: done up to line 50

--- Garden Advisor.py
+++ Garden Advisor.py
@@ -21,3 +21,31 @@ Context: Crop: {crop}, County: {county}, Problem: {problem}. Assume limited budg
 Constraints: Only list agricultural related causes. Do not respond to anything that is not related to the topic of agriculture, instead respond with "I can only provide agricultural advice, would you like me to continue with that?"
 Output: Return only valid json, with this exact format: {{"likely_causes": ["cause1", "cause2", "cause3"]}} and {{"possible_solutions": ["solution1", "solution2", "solution3"]}}. Do not include any other text or explanation, and provide the answers in point form. If you cannot provide 3 causes or solutions, please provide as many as you can, but do not make up any causes or solutions.
 """
+
+# call the Gemini API for stage 1
+response = client.models.generate_content(
+    model=model,
+    contents=stage1_prompt,
+)
+print("/n=== Stage 1: Diagnosis of the garden problem ===/n")
+print(response.text)
+
+stage1_result = response.text
+
+#stage 2 prompt - generate a detailed action plan based on the diagnosis
+stage2_prompt = f"""
+Role: You're an agricultural expert for small scale farmers in Kenya.
+Task: Generate a detailed action plan based on the diagnosis from stage 1.
+Context: Crop: {crop}, County: {county}, Problem: {problem}
+constraints: Only provide agricultural advice. Do not respond to anything that is not related to the topic of agriculture, instead respond with "I can only provide agricultural advice, would you like me to continue with that?"
+Output: Return only valid json, with this exact format: {{"action_plan": ["step1", "step2", "step3"]}}. Do not include any other text or explanation, and provide the answers in point form. If you cannot provide 3 steps, please provide as many as you can, but do not make up any steps.
+"""
+
+# call the Gemini API for stage 2
+response2 = client.models.generate_content(
+    model=model,
+    contents=stage2_prompt
+)
+print("/n=== Stage 2: solutions of Diagnosis ===/n")
+print(response2.text)
+
